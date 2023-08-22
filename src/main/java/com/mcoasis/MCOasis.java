81862683e8ef178b236d96613e7baac81f2ccb74
EthanGarey <ethgarey@gmail.com>
2023-08-22 18:00:35 -0400

    Commit

--- src/main/java/com/mcoasis/MCOasis.java
+++ src/main/java/com/mcoasis/MCOasis.java
@@ -1,5 +1,6 @@
 package com.mcoasis;
 
+import com.mcoasis.events.playerEvents;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class MCOasis extends JavaPlugin {
@@ -7,7 +8,7 @@ public final class MCOasis extends JavaPlugin {
     @Override
     public void onEnable() {
         // Plugin startup logic
-
+        new playerEvents(this);
     }
 
     @Override
