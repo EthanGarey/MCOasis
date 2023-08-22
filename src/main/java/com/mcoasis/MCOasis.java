3ab7867ba5e961bad99c7c7e978d579d7e7b4d7b
EthanGarey <ethgarey@gmail.com>
2023-08-22 17:51:12 -0400

    First Commit

---
+++ src/main/java/com/mcoasis/MCOasis.java
@@ -0,0 +1,17 @@
+package com.mcoasis;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class MCOasis extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
